bf0ffcbf20d44a56c0c5d5f40617e26ea963d77a
Vijay Solanki <v2solanki@gmail.com>
2020-06-04 22:59:59 +0530

    first commit

---
+++ src/Vjsel/DemoTest.java
@@ -0,0 +1,16 @@
+package Vjsel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver");
+		WebDriver driver =new ChromeDriver();
+		
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,5 @@
+module JavaSeleniumDemo {
+	requires okio;
+	requires org.openqa.selenium.core;
+	requires selenium.chrome.driver;
+}
